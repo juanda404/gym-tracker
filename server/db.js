aec2e5bd10cf1fafbb29849cc8e41a9e89744a90
Juan David Santamaría <juanda404@gmail.com>
2025-07-10 12:59:50 -0500

    Feat: add CRUD API for routines using Supabase SDK

--- server/db.js
+++ server/db.js
@@ -1,15 +1,18 @@
-const { pool } = require('pg')
-require('dotenv').config()
+import dotenv from 'dotenv'
+import pg from 'pg'
 
-const pool = new poo({
+dotenv.config()
+const { Pool } = pg
+
+const pool = new Pool({
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
     ssl:{
-        rejectUnauthorized: false
-    }
+        rejectUnauthorized: false,
+    },
 })
 
-module.exports = pool
+export const query = (text, params) => pool.query(text, params)
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const cors = require('cors')
-const pool = require('./db')
+
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import supabase from './supabaseClient.js'
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -8,20 +10,63 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+// Ruta base
 app.get('/', (req,res) =>{
-    res.send('🚀 API is running')
+    res.send('🚀 API is running with Supabase SDK!!')
 })
 
-// Nueva ruta para obtener rutinas
+// 1. Obtener todas las rutinas
 app.get('/routines', async (req,res) =>{
-    try{
-        const result = await pool.query('SELECT * FROM routines ORDER BY id')
-        res.json(result.rows)
-    }catch(err){
-        console.error(err.message)
-        res.status(500).send('Error retrieving routines')
-    }
+    const {data, error} = await supabase
+    .from('routines')
+    .select('*')
+    .order('id')
+        
+    if (error) return res.status(500).json({ error: error.message})
+    res.json(data)
 })
+
+// 2. Crear una nueva rutina
+app.post('/routines',async(req,res)=>{
+    const {name,focus,duration,exercises} = req.body
+
+    const {data, error} = await supabase
+    .from('routines')
+    .insert([ {name, focus, duration, exercises}])
+    .select()
+    if (error) return res.status(500).json({error:  error.message})
+    res.status(201).json(data[0])
+})
+
+// 3. actualizar rutina
+app.put('/routines/:id', async (req,res)=>{
+    const {id} = req.params
+    const { name, focus, duration, exercises} = req.body
+
+    const { data, error } = await supabase
+    .from('routines')
+    .update({name, focus, duration, exercises})
+    .eq('id', id)
+    .select()
+
+    if (error) return res.status(500).json({error:  error.message})
+    res.status(201).json(data[0])
+})
+
+//4. Eliminar rutina
+app.delete('/routines/:id', async (req, res) => {
+  const { id } = req.params
+
+  const { error } = await supabase
+    .from('routines')
+    .delete()
+    .eq('id', id)
+
+  if (error) return res.status(500).json({ error: error.message })
+  res.status(204).send()
+})
+
+//iniciar servidor
 
 app.listen(PORT, ()=>{
     console.log(`Server running on http://localhost:${PORT}`)
